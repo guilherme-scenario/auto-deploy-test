8555f43ed0611b01cb2a06a85acac3df6dc92198
guilherme-scenario <guilherme@scenario.ind.br>
2023-07-03 11:31:54 -0300

    testing GitHub secrets

--- src/main/java/com/test/autodeploytest/Controller/HelloWorldController.java
+++ src/main/java/com/test/autodeploytest/Controller/HelloWorldController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello")
 public class HelloWorldController {
 
-    @Value("${test}")
+    @Value("${aws.secretKey}")
     private String envTest;
 
     @GetMapping
