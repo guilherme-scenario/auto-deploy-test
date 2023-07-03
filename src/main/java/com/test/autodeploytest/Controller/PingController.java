6b58157a997d05b4c919d984b0e09a79c0474731
guilherme-scenario <guilherme@scenario.ind.br>
2023-07-03 13:13:41 -0300

    testing GitHub secrets

---
+++ src/main/java/com/test/autodeploytest/Controller/PingController.java
@@ -0,0 +1,18 @@
+package com.test.autodeploytest.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ping")
+public class PingController {
+
+
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
