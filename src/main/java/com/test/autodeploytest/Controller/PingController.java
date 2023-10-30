fa434c9a82c67b356095168769b8bd5cbccf02a7
Guilherme Sousa <guilherme@scenario.ind.br>
2023-10-30 16:03:55 -0300

    test start-deploy

--- src/main/java/com/test/autodeploytest/Controller/PingController.java
+++ src/main/java/com/test/autodeploytest/Controller/PingController.java
@@ -14,6 +14,6 @@ public class PingController {
     @GetMapping
     public ResponseEntity<String> ping() {
 
-        return ResponseEntity.ok("pong");
+        return ResponseEntity.ok("pong 2");
     }
 }
