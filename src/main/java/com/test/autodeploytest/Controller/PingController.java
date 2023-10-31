2dbec7d3a202a344d9f85dbaffd4e612a7698270
Guilherme Sousa <guilherme@scenario.ind.br>
2023-10-31 11:56:21 -0300

    test start-deploy

--- src/main/java/com/test/autodeploytest/Controller/PingController.java
+++ src/main/java/com/test/autodeploytest/Controller/PingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("ping")
+@RequestMapping("v2/ping")
 public class PingController {
 
 
