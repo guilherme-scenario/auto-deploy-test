3b2f1d2a338f4d8ef1fcc135165cb365a218b15a
Guilherme Sousa <guilherme@scenario.ind.br>
2024-02-02 09:44:16 -0300

    test start-deploy

--- src/main/java/com/test/autodeploytest/AutoDeployTestApplication.java
+++ src/main/java/com/test/autodeploytest/AutoDeployTestApplication.java
@@ -3,7 +3,7 @@ package com.test.autodeploytest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AutoDeployTestApplication {
 
 	public static void main(String[] args) {
